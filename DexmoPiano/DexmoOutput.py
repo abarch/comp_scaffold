8382bf8d9bb2cf291a37b9a9cba2c8a92853b210
jseidel <jseidel@techfak.uni-bielefeld.de>
2020-07-06 18:22:02 +0200

    Add GUI and DexmoOutput for demo tasks

---
+++ DexmoPiano/DexmoOutput.py
@@ -0,0 +1,65 @@
+import mido
+from mido import Message
+from mido import MidiFile
+import time
+
+import curses
+import datetime
+
+stdscr = curses.initscr()
+curses.noecho()
+stdscr.nodelay(1) # set getch() non-blocking
+
+
+# FIXME: this needs to be adapted
+midi_interface = 'DEXMO_R:DEXMO_R MIDI 1 20:0'
+midi_interface_sound = 'Midi Through:Midi Through Port-0 14:0'
+
+# abstract in python of the MIDI_HAPTIC_DEFINITION
+# define channel of the device here
+CHAN = 9
+# choosing action on the index here
+MHP_ACT_IND = 36
+# various action modes
+NOTE_E= 4
+NOTE_F= 5
+NOTE_A= 9
+
+# Send an action to the haptic device over the midi interface
+def haptic_action(char):
+    with mido.open_output(midi_interface) as outport:
+
+        if char == 'i': # inwards dexmo impulse
+            msg = Message('note_on', channel=CHAN, note=MHP_ACT_IND+NOTE_F, velocity=50)
+            outport.send(msg)
+        elif char == 'o': # outwards dexmo impulse
+            msg = Message('note_off', channel=CHAN, note=MHP_ACT_IND+NOTE_F, velocity=50)
+            outport.send(msg)
+
+# play demo: sound output of notes and haptic feedback impulse
+def play_demo(midiFile):
+    with mido.open_output(midi_interface_sound) as port:
+        for msg in MidiFile(midiFile).play():
+            port.send(msg)       # sound from piano and metronome track
+            if msg.channel == 0: # haptic feeback for notes in Piano track
+                if msg.type == 'note_on':
+                    haptic_action('i')
+                elif msg.type == 'note_off':
+                    haptic_action('o')
+
+# only haptic feedback impulse
+def practice_task(midiFile):
+    with mido.open_output(midi_interface_sound) as port:
+        for msg in MidiFile(midiFile).play():
+            if msg.channel == 9:
+                port.send(msg)      # sound only from metronome track
+            if msg.channel == 0:    # haptic feeback for notes in Piano track
+                if msg.type == 'note_on':
+                    haptic_action('i')
+                elif msg.type == 'note_off':
+                    haptic_action('o')
+
+if __name__ == '__main__':
+    #start_music()
+    play_demo()
+    # practice_task()
---
+++ DexmoPiano/DexmoPianoGui.py
@@ -0,0 +1,90 @@
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image, ImageDraw, ImageFilter, ImageTk
+import numpy as py
+import time
+import os
+import DexmoOutput
+import MidiGen
+
+
+# starts only metronome output and haptic impulse from dexmo for every note
+# TODO generate personal user feedback in DexmoOutput, depending on errors
+def startTask():
+    DexmoOutput.practice_task('output.mid')
+
+
+# starts Demo with sound output and haptic impulse from dexmo for every note
+def startDemo():
+    DexmoOutput.play_demo('output.mid')
+
+
+# generate new midiFile and Notesheet and displays it
+# TODO generate MidiFile with arguments out of errors
+def nextTask():
+    MidiGen.generateMidi()
+    os.system('midi2ly output.mid')
+    os.system('lilypond -fpng output-midi.ly')
+    clearFrame()
+    load_Notesheet("output-midi.png")
+    load_taskButtons()
+
+# loads notesheet for actual task
+def load_Notesheet(png):
+    global background
+    background = Image.open(png)
+    background = background.convert("RGBA")
+    global width, height
+    width, height = background.size
+
+    img = ImageTk.PhotoImage(background)
+    # create a label
+    panel = Label(root, image = img)
+    # set the image as img
+    panel.image = img
+    panel.pack(side = RIGHT)
+
+# create button for demo, practicing, next task, back to start menu
+def load_taskButtons():
+    startMenuButton = Button(root, text ='Back to Menu',
+        command = backToMenu).place(x = 10, y = 940, height=50, width=150)
+    startTaskcButton = Button(root, text ='Start Task',
+        command = startTask).place(x = 10, y = 100, height=50, width=150)
+    startDemoButton = Button(root, text ='Start Demo',
+        command = startDemo).place(x = 10, y = 160, height=50, width=150)
+    nextTaskButton = Button(root, text ='Next Task',
+        command = nextTask).place(x = 10, y = 400, height=50, width=150)
+
+
+# load start menu with button for first task and exit button
+def load_Startmenu():
+    nextTaskButton = Button(root, text ='Start first task',
+        command = nextTask).place(x = 425, y = 440, height=50, width=150)
+    startDemoButton = Button(root, text ='Quit',
+        command = quit).place(x = 425, y = 500, height=50, width=150)
+
+# destroy all widgets from frame
+def clearFrame():
+    for widget in root.winfo_children():
+       widget.destroy()
+
+# go back to start menu
+def backToMenu():
+    clearFrame()
+    load_Startmenu()
+
+# wuit "Piano with dexmo"
+def quit():
+    root.destroy()
+
+##_____________________________START LOOP HERE________________________________##
+
+# Create a window and title
+root = Tk()
+root.title("Piano with Dexmo")
+
+load_Startmenu()
+# Set the resolution of window
+root.geometry("1000x1000")
+
+root.mainloop()
