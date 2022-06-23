d266b15e1c06c8804cac91deb3ce09e0685b524e
maorros <maorros@gmail.com>
2022-06-23 15:43:29 +0300

    add mac name to computers that work with Nord

--- DexmoPiano/settings.py
+++ DexmoPiano/settings.py
@@ -2,7 +2,9 @@ import socket
 # computer specific settings
 
 # TAU lab computer
-if socket.gethostname() == 'DESKTOP-17671':
+
+print(socket.gethostname())
+if socket.gethostname() == 'DESKTOP-17671' or socket.gethostname() == 'Homs-MacBook-Air.local':
     ### CONSTANTS ###
     CHANNEL_PIANO = 0
     CHANNEL_METRO = 0  # for Nord 4, metronome has to be on the same channel
